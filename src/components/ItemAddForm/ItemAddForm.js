1bd8d67d132ac6a313732f46051894dd60083e73
Andrii <circumflexus@protonmail.com>
2021-11-09 20:04:08 +0200

    Added ItemAddForm feature

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -2,16 +2,40 @@ import React from "react";
 
 import './ItemAddForm.css';
 
-const ItemAddForm = ({addItem}) => {
-    return(
-        <div className="item-add-form">
-            <button type="button"
-                    className="btn btn-outline-secondary"
-                    onClick={() => addItem('Test')}>
-                Add item
-            </button>
-        </div>
-    );
-};
+export default class ItemAddForm extends React.Component {
 
-export default ItemAddForm;
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addItem(this.state.label);
+        this.setState({
+            label: ''
+        });
+    };
+
+    render() {
+        return(
+            <form className="item-add-form d-flex"
+                    onSubmit={this.onSubmit}>
+                <input type="text"
+                        className="form-control"
+                        onChange={this.onLabelChange}
+                        placeholder="Add something"
+                        value={this.state.label} />
+                <button type="submit"
+                        className="btn btn-outline-secondary">
+                    +
+                </button>
+            </form>
+        );
+    }
+};
